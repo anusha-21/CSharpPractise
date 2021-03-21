03dd7ea13ce503eb132ca63a7ad6310761403251
Anusha <anusha211196@gmail.com>
2021-03-21 15:51:08 +0530

    Initial commit

---
+++ SchoolLibrary/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public abstract class Person //it cannot be directly instantiated,means that an object cannot be created from it.
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ SchoolLibrary/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolLibrary
+{
+    public class Student : Person
+    {
+        public enum GradeLevels { Ninth, Tenth, PlusOne, PlusTwo }
+        public GradeLevels GradeLevel { get; set; }
+    }
+}
